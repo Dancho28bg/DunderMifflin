d4bbe3cfb4109421452dce8e1ce662ca96b30d01
Dancho28bg <y.rusev28@gmail.com>
2024-11-02 22:26:14 +0100

    ReadME file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-DunderMifflin Compulsory Assignment.
-"npm run dev" in console to run the app
+**DunderMifflin Compulsory Assignment.**
+Hi Alex,
+So i tried to fix my app but I encounter some errors and spend last 2 days trying to fix, but I still cannot. I am really at a dead end and cannot fix it, of course on Monday I will ask you face to face about them.
+
+To run the app:
+(for backend)
+docker compose up --build
+cd backend
+dotnet ef database update
+dotnet run
+(for frontend)
+npm run dev
--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccaa616f1f083491679181a52d0ca59b5bf51d8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e08d39004b02ac92c54b7ffb96777e647b16eac")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
